bb0ea8ba5f1a93ed0ea94e00c281f0a6581ed14e
KYIV\Anastasiia_Sokolova <Anastasiia_Sokolova@epam.com>
2018-07-31 16:49:51 +0300

    calling to the api using promises

--- serverCalls/js/script.js
+++ serverCalls/js/script.js
@@ -4,15 +4,16 @@ var key = '?api_key=f3754f54a647c3b3e1a8cc28028c910b',
 
 
 /*==== XMLHttpRequest case ====*/
+
 function xmlRequest(url, number) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.send();
 
     xhr.onreadystatechange = function() {
-        if (xhr.readyState != 4) return;
+        if (this.readyState != 4) return;
 
-        if (xhr.status != 200) {
+        if (this.status != 200) {
             console.log('sth goes wrong')
         } else {
             var response = JSON.parse(xhr.response);
@@ -42,3 +43,42 @@ function xmlRequest(url, number) {
 
 xmlRequest('https://api.themoviedb.org/3/movie/' + movie_id + key, 1);
 
+
+
+/*==== Promises case ====*/
+
+function promiseRequest(url) {
+
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.send();
+        xhr.onreadystatechange = function() {
+            if (this.readyState != 4) return;
+
+            if (xhr.status != 200) {
+                var error = new Error(this.statusText);
+                reject(error);
+            } else {
+                resolve(JSON.parse(this.response))
+            }
+        }
+    })
+}
+
+promiseRequest('https://api.themoviedb.org/3/movie/' + movie_id + key)
+    .then(function(response) {
+          document.getElementById('movie-name').innerText = response.title;
+          return promiseRequest('https://api.themoviedb.org/3/movie/' + movie_id + '/credits' + key)
+    }).then(function(response) {
+          document.getElementById('team-size').innerText = response.crew.length;
+          var person_id = (response.crew[0]).id;
+          return promiseRequest('https://api.themoviedb.org/3/person/' + person_id + key);
+     }).then(function(response) {
+          document.getElementById('member-name').innerText = response.name;
+          image_path = response.profile_path;
+          document.getElementById('member-picture').src = 'https://image.tmdb.org/t/p/w600_and_h900_bestv2' + image_path + key;
+     }).catch(function(error) {
+          console.log(error);
+});
